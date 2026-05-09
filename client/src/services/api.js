d5f035c02ff1ba5a1c21d8a972990fabbfdf2dcb
Alok Raj <aloksingh0565@gmail.com>
2026-05-09 23:34:50 +0530

    fixed production api

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   headers: {
-    "Content-Type": "application/json"
-  }
+    "Content-Type": "application/json",
+  },
 });
 
 export default API;
